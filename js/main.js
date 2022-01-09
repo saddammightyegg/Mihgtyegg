011d1cba59d16d75e9e8b0b120f93aa1eafd36ae
saddam <xyzsaddam2013@gmail.com>
2022-01-09 17:35:50 +0600

    My first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+    let text = "";
+    for (let i = 0; i < 10; i++) {
+    if (i === 3) { break; }
+    text += "The number is " + i + "<br>";
+    }
+
+    document.getElementById("demo").innerHTML = text;
